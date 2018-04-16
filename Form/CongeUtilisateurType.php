ca3d89c814e0407e2dba11f047deafe0ac49d47a
dsingh <dsingh@inter-invest.fr>
2018-04-16 10:17:38 +0200

    type

--- Form/CongeUtilisateurType.php
+++ Form/CongeUtilisateurType.php
@@ -3,6 +3,7 @@
 namespace InterInvest\CongesBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
